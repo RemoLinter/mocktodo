2ec014bdcd85cbca09df915acf1217bc50a28938
Remo Linter <remo.linter@look4.li>
2017-02-25 11:48:26 +0100

    PHP Dateien hinzugefügt

--- api/alleTasks.php
+++ api/alleTasks.php
@@ -2,27 +2,28 @@
 
 $tasks = [
     [
-        "caption" => "aaa",
-        "status" => "open",
-        "id" => "77c5a541-bb39-4644-94b4-bffd46c14a0e"
+        'caption' => 'Bier',
+        'status' => 'open',
+        'id' => '77c5a541-bb39-4644-94b4-bffd46c14a0e'
     ],
     [
-        "caption" => "bbb",
-        "status" => "closed",
-        "id" => "25c9f45b-0008-4960-acd3-363249b0536d"
+        'caption' => 'Wäsche',
+        'status' => 'closed',
+        'id' => '25c9f45b-0008-4960-acd3-363249b0536d'
     ],
     [
-        "caption" => "ccc",
-        "status" => "open",
-        "id" => "544cb393-b29e-4bc9-829d-6c57eee4877d"
+        'caption' => 'Milch',
+        'status' => 'open',
+        'id' => '544cb393-b29e-4bc9-829d-6c57eee4877d'
     ],
     [
-        "caption" => "ddd",
-        "status" => "closed",
-        "id" => "d1ac6644-b331-478e-ba51-1280a336f8d5"
+        'caption' => 'Brot',
+        'status' => 'closed',
+        'id' => 'd1ac6644-b331-478e-ba51-1280a336f8d5'
     ]
 ];
 
-header("Content-Type: application/json");
+header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json');
 
-echo json_encode($tasks);
+echo json_encode($tasks, JSON_UNESCAPED_UNICODE);
